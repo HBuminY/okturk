a440b2f67e12736705fe8e796d763bdde78dfd2d
Hulusi Bumin Yalçın <pybhad@gmail.com>
2023-12-13 12:11:08 +0300

    price table

--- sakultaweb/src/lib/PriceTable.svelte
+++ sakultaweb/src/lib/PriceTable.svelte
@@ -1,15 +1,22 @@
-<table class="table-fixed border-collapse m-4 text-center bg-white
+<table class="table-fixed border-collapse m-8 text-center font-rb text-lg shadow-2xl translate-x-4
     [&>*>*>*]:border-4
     [&>*>*>*]:px-4
     [&>*>*>*]:py-2
-    [&>*>*>*]:border-black  
+    [&>*>*>*]:border-black
+    [&>*>*>*]:bg-white
 ">
     <thead>
         <tr>
-            <th>Özellik</th>
-            <th>QR Menu</th>
-            <th>Kurumsal Website</th>
-            <th>Kurumsal Dinamik Website</th>
+            <th class="invisible border-none">Özellik</th>
+            <th class="!bg-orange-100">QR Menu</th>
+            <th class="!bg-orange-200 underline">Kurumsal Website</th>
+            <th class="!bg-yellow-500 underline decoration-double">Kurumsal Dinamik Website</th>
+        </tr>
+        <tr class="text-sm [&>*]:max-w-xs">
+            <th class="invisible border-none">Özellik</th>
+            <th class="!bg-orange-100">İşletmeniz için düşük maliyetli yüksek kalite QR menü</th>
+            <th class="!bg-orange-200">Kurumunuzun internet varlığını sağlayacak harika web tasarımları</th>
+            <th class="!bg-yellow-500">Kurumunuzun bütün web ihtiyaçlarına karşılık gelecek sunucu destekli uygun web sistemleri</th>
         </tr>
     </thead>
     
@@ -25,6 +32,12 @@
             <td>yok</td>
             <td>4</td>
             <td>5</td>
+        </tr>
+        <tr>
+            <td>7/24 Destek</td>
+            <td>yok</td>
+            <td>yok</td>
+            <td>var</td>
         </tr>
         <tr>
             <td>Sunucu Hizmeti</td>
